fb2fc665139005b64d805dd8b0c7ed798203f272
Sandy Edwards <sedwardscode@gmail.com>
2018-08-27 12:55:54 -0400

    first commit

---
+++ src/Components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Login = (props) => {
+
+  const visitSpotifyLogin = () => {
+    window.location='http://localhost:3000/api/v1/login';
+  }
+
+  return (
+    <div className="login-container">
+      <div onClick={visitSpotifyLogin} className="spotify-login-button" >
+      </div>
+    </div>
+  )
+};
+
+
+export default Login;
