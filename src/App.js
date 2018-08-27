53a3b057f824f1648759296c5b7aa647bf7d4e2e
Sandy Edwards <sedwardscode@gmail.com>
2018-08-27 14:38:06 -0400

    added logo to header, began creating selector for choosing genre

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login'
+import GenreSelector from './Components/GenreSelector'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src='v-sample-logo.png' className="App-logo" alt="logo" />
+          <h1 className="App-title">VibeList</h1>
         </header>
       <Login />
+      <GenreSelector />
       </div>
     );
   }
---
+++ src/Components/GenreSelector.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const GenreSelector = (props) => {
+
+
+  return (
+    <div className="genre-selector-container">
+      Choose a Genre:
+      <form>
+      <select>
+      <option key={null}>test</option>
+      </select>
+      </form>
+    </div>
+  )
+};
+
+
+export default GenreSelector;
