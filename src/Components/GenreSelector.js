b7c09408345eb2dd4dff7a3d12a9b2f437625924
Sandy Edwards <sedwardscode@gmail.com>
2018-08-28 09:58:54 -0400

    changed genre selector to mood selector

--- src/Components/GenreSelector.js
+++ src/Components/GenreSelector.js
@@ -1,163 +1,45 @@
 import React from 'react';
 import uuid from 'uuid'
+import genres from '../Genres'
 
 class GenreSelector extends React.Component {
 
   state = {
-    selectedGenre: 'acoustic'
+    selectedMood: 'sad'
   }
 
-  genres = [
-    "acoustic",
-    "afrobeat",
-    "alt-rock",
-    "alternative",
-    "ambient",
-    "anime",
-    "black-metal",
-    "bluegrass",
-    "blues",
-    "bossanova",
-    "brazil",
-    "breakbeat",
-    "british",
-    "cantopop",
-    "chicago-house",
-    "children",
-    "chill",
-    "classical",
-    "club",
-    "comedy",
-    "country",
-    "dance",
-    "dancehall",
-    "death-metal",
-    "deep-house",
-    "detroit-techno",
-    "disco",
-    "disney",
-    "drum-and-bass",
-    "dub",
-    "dubstep",
-    "edm",
-    "electro",
-    "electronic",
-    "emo",
-    "folk",
-    "forro",
-    "french",
-    "funk",
-    "garage",
-    "german",
-    "gospel",
-    "goth",
-    "grindcore",
-    "groove",
-    "grunge",
-    "guitar",
-    "happy",
-    "hard-rock",
-    "hardcore",
-    "hardstyle",
-    "heavy-metal",
-    "hip-hop",
-    "holidays",
-    "honky-tonk",
-    "house",
-    "idm",
-    "indian",
-    "indie",
-    "indie-pop",
-    "industrial",
-    "iranian",
-    "j-dance",
-    "j-idol",
-    "j-pop",
-    "j-rock",
-    "jazz",
-    "k-pop",
-    "kids",
-    "latin",
-    "latino",
-    "malay",
-    "mandopop",
-    "metal",
-    "metal-misc",
-    "metalcore",
-    "minimal-techno",
-    "movies",
-    "mpb",
-    "new-age",
-    "new-release",
-    "opera",
-    "pagode",
-    "party",
-    "philippines-opm",
-    "piano",
-    "pop",
-    "pop-film",
-    "post-dubstep",
-    "power-pop",
-    "progressive-house",
-    "psych-rock",
-    "punk",
-    "punk-rock",
-    "r-n-b",
-    "rainy-day",
-    "reggae",
-    "reggaeton",
-    "road-trip",
-    "rock",
-    "rock-n-roll",
-    "rockabilly",
-    "romance",
-    "sad",
-    "salsa",
-    "samba",
-    "sertanejo",
-    "show-tunes",
-    "singer-songwriter",
-    "ska",
-    "sleep",
-    "songwriter",
-    "soul",
-    "soundtracks",
-    "spanish",
-    "study",
-    "summer",
-    "swedish",
-    "synth-pop",
-    "tango",
-    "techno",
-    "trance",
-    "trip-hop",
-    "turkish",
-    "work-out",
-    "world-music"
+  moods = [
+    'sad',
+    'content',
+    'ecstatic'
   ]
 
   renderGenreOptions = () => {
-    return this.genres.map(genre => <option key={uuid()}>{genre}</option>)
+    return genres.map(genre => <option key={uuid()}>{genre}</option>)
+  }
+
+  renderMoodOptions = () => {
+    return this.moods.map(mood => <option key={uuid()}>{mood}</option>)
   }
 
   handleChange = (event) => {
     this.setState({
-      selectedGenre: event.target.value
+      selectedMood: event.target.value
     })
   }
 
   handleSubmit = (event) => {
     event.preventDefault()
-    window.location='http://localhost:3000/api/v1/search/?genre=' + this.state.selectedGenre
+    window.location='http://localhost:3000/api/v1/search/?mood=' + this.state.selectedMood
   }
 
   render(){
     return (
       <div className="genre-selector-container">
-        Choose a Genre:
+        Choose a Mood:
         <form className="genre-form" onSubmit={this.handleSubmit}>
-        <select onChange={this.handleChange} value={this.state.selectedGenre}>
-        {this.renderGenreOptions()}
+        <select onChange={this.handleChange} value={this.state.selectedMood}>
+        {this.renderMoodOptions()}
         </select>
         <input type="submit" value="submit" />
         </form>
