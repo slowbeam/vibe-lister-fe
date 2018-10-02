51541c63240fe9832a438c3ca491f2cf4eba7d15
Sandy <sedwardscode@gmail.com>
2018-10-02 16:33:28 -0400

    fixed merge conflict

--- src/apis/adapter.js
+++ src/apis/adapter.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-const SONG_URL = 'https://vibelist-server.herokuapp.com/api/v1/songs'
-const USER_URL = 'https://vibelist-server.herokuapp.com/api/v1/users'
-const SONG_USER_URL = 'https://vibelist-server.herokuapp.com/api/v1/song_users'
-const MOOD_URL = 'https://vibelist-server.herokuapp.com/api/v1/moods'
-const LOGGED_IN_USER_URL = 'https://vibelist-server.herokuapp.com/api/v1/logged-in-user'
-=======
 const SONG_URL = 'http://localhost:3000/api/v1/songs'
 const USER_URL = 'http://localhost:3000/api/v1/users'
 const SONG_USER_URL = 'http://localhost:3000/api/v1/song_users'
 const MOOD_URL = 'http://localhost:3000/api/v1/moods'
 const LOGGED_IN_USER_URL = 'http://localhost:3000/api/v1/logged-in-user'
 const MOOD_SEARCH_URL = 'http://localhost:3000/api/v1/search-two'
->>>>>>> no_refresh
 
 const token = localStorage.getItem('jwt')
 
