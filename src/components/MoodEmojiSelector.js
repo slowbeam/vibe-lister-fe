2d36b94e3ca7392ba1abe6693663ffeb3a7eed2f
Sandy <sedwardscode@gmail.com>
2019-02-04 11:44:05 -0500

    switched all localhost links to live server

--- src/components/MoodEmojiSelector.js
+++ src/components/MoodEmojiSelector.js
@@ -1,94 +1,158 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import withAuth from '../hocs/withAuth';
-import GenreSelectDialog from './GenreSelectDialog';
-import SelectedGenresList from './SelectedGenresList';
-import * as actions from '../actions';
-import { withRouter } from 'react-router';
-import { compose } from 'redux';
-
-
+import React from "react";
+import { connect } from "react-redux";
+import withAuth from "../hocs/withAuth";
+import GenreSelectDialog from "./GenreSelectDialog";
+import SelectedGenresList from "./SelectedGenresList";
+import * as actions from "../actions";
+import { withRouter } from "react-router";
+import { compose } from "redux";
 
 class MoodEmojiSelector extends React.Component {
-
-
-  handleSubmit = (mood) => {
-    
-    if (this.props.genreOne !== undefined && this.props.genreTwo !== undefined && this.props.genreThree !== undefined ){
-
-      const fetchUrl = 'http://localhost:3000/api/v1/search/?mood=' + mood + "&genreone=" + this.props.genreOne + "&genretwo=" + this.props.genreTwo + "&genrethree=" + this.props.genreThree
+  handleSubmit = mood => {
+    if (
+      this.props.genreOne !== undefined &&
+      this.props.genreTwo !== undefined &&
+      this.props.genreThree !== undefined
+    ) {
+      const fetchUrl =
+        "https://vibelist-server.herokuapp.com/api/v1/search/?mood=" +
+        mood +
+        "&genreone=" +
+        this.props.genreOne +
+        "&genretwo=" +
+        this.props.genreTwo +
+        "&genrethree=" +
+        this.props.genreThree;
       this.props.fetchMoodSearch(fetchUrl);
-      this.props.history.push('/current-vibelist')
-    }
-    else if (this.props.genreOne !== undefined && this.props.genreTwo !== undefined && this.props.genreThree === undefined) {
-      const fetchUrl = 'http://localhost:3000/api/v1/search/?mood=' + mood + "&genreone=" + this.props.genreOne + "&genretwo=" + this.props.genreTwo;
+      this.props.history.push("/current-vibelist");
+    } else if (
+      this.props.genreOne !== undefined &&
+      this.props.genreTwo !== undefined &&
+      this.props.genreThree === undefined
+    ) {
+      const fetchUrl =
+        "https://vibelist-server.herokuapp.com/api/v1/search/?mood=" +
+        mood +
+        "&genreone=" +
+        this.props.genreOne +
+        "&genretwo=" +
+        this.props.genreTwo;
       this.props.fetchMoodSearch(fetchUrl);
-      this.props.history.push('/current-vibelist')
-    }
-
-    else if (this.props.genreOne !== undefined && this.props.genreTwo === undefined && this.props.genreThree === undefined ) {
-      const fetchUrl = 'http://localhost:3000/api/v1/search/?mood=' + mood + "&genreone=" + this.props.genreOne;
+      this.props.history.push("/current-vibelist");
+    } else if (
+      this.props.genreOne !== undefined &&
+      this.props.genreTwo === undefined &&
+      this.props.genreThree === undefined
+    ) {
+      const fetchUrl =
+        "https://vibelist-server.herokuapp.com/api/v1/search/?mood=" +
+        mood +
+        "&genreone=" +
+        this.props.genreOne;
       this.props.fetchMoodSearch(fetchUrl);
-      this.props.history.push('/current-vibelist')
-    }
-    else if (this.props.genreOne !== undefined && this.props.genreTwo === undefined && this.props.genreThree !== undefined) {
-        const fetchUrl = 'http://localhost:3000/api/v1/search/?mood=' + mood + "&genreone=" + this.props.genreOne + "&genrethree=" + this.props.genreThree;
-        this.props.fetchMoodSearch(fetchUrl);
-        this.props.history.push('/current-vibelist')
-    }
-    else if (this.props.genreOne === undefined && this.props.genreTwo !== undefined && this.props.genreThree !== undefined) {
-        const fetchUrl = 'http://localhost:3000/api/v1/search/?mood=' + mood + "&genretwo=" + this.props.genreTwo + "&genrethree=" + this.props.genreThree;
-        this.props.fetchMoodSearch(fetchUrl);
-        this.props.history.push('/current-vibelist')
-    }
-    else if (this.props.genreOne === undefined && this.props.genreTwo !== undefined && this.props.genreThree === undefined) {
-        const fetchUrl = 'http://localhost:3000/api/v1/search/?mood=' + mood + "&genretwo=" + this.props.genreTwo;
-        this.props.fetchMoodSearch(fetchUrl);
-        this.props.history.push('/current-vibelist')
-    }
-    else if (this.props.genreOne === undefined && this.props.genreTwo === undefined && this.props.genreThree !== undefined) {
-        const fetchUrl = 'http://localhost:3000/api/v1/search/?mood=' + mood + "&genrethree=" + this.props.genreThree;
-        this.props.fetchMoodSearch(fetchUrl);
-        this.props.history.push('/current-vibelist')
-    }
-    else {
-
-      const fetchUrl = `http://localhost:3000/api/v1/search/?mood=${mood}`;
+      this.props.history.push("/current-vibelist");
+    } else if (
+      this.props.genreOne !== undefined &&
+      this.props.genreTwo === undefined &&
+      this.props.genreThree !== undefined
+    ) {
+      const fetchUrl =
+        "https://vibelist-server.herokuapp.com/api/v1/search/?mood=" +
+        mood +
+        "&genreone=" +
+        this.props.genreOne +
+        "&genrethree=" +
+        this.props.genreThree;
       this.props.fetchMoodSearch(fetchUrl);
-      this.props.history.push('/current-vibelist')
+      this.props.history.push("/current-vibelist");
+    } else if (
+      this.props.genreOne === undefined &&
+      this.props.genreTwo !== undefined &&
+      this.props.genreThree !== undefined
+    ) {
+      const fetchUrl =
+        "https://vibelist-server.herokuapp.com/api/v1/search/?mood=" +
+        mood +
+        "&genretwo=" +
+        this.props.genreTwo +
+        "&genrethree=" +
+        this.props.genreThree;
+      this.props.fetchMoodSearch(fetchUrl);
+      this.props.history.push("/current-vibelist");
+    } else if (
+      this.props.genreOne === undefined &&
+      this.props.genreTwo !== undefined &&
+      this.props.genreThree === undefined
+    ) {
+      const fetchUrl =
+        "https://vibelist-server.herokuapp.com/api/v1/search/?mood=" +
+        mood +
+        "&genretwo=" +
+        this.props.genreTwo;
+      this.props.fetchMoodSearch(fetchUrl);
+      this.props.history.push("/current-vibelist");
+    } else if (
+      this.props.genreOne === undefined &&
+      this.props.genreTwo === undefined &&
+      this.props.genreThree !== undefined
+    ) {
+      const fetchUrl =
+        "https://vibelist-server.herokuapp.com/api/v1/search/?mood=" +
+        mood +
+        "&genrethree=" +
+        this.props.genreThree;
+      this.props.fetchMoodSearch(fetchUrl);
+      this.props.history.push("/current-vibelist");
+    } else {
+      const fetchUrl = `https://vibelist-server.herokuapp.com/api/v1/search/?mood=${mood}`;
+      this.props.fetchMoodSearch(fetchUrl);
+      this.props.history.push("/current-vibelist");
     }
-  }
+  };
 
-  render(){
+  render() {
     return (
       <div className="mood-selector-container">
-
         <GenreSelectDialog />
         <SelectedGenresList />
         <h4>Choose a Mood:</h4>
         <div className="mood-emoji-container">
-        <img onClick={() => this.handleSubmit('sad')} alt="" src="/images/emojis/sad-2.png"/>
-        <img onClick={() => this.handleSubmit('content')} alt="" src="/images/emojis/content-2.png"/>
-        <img onClick={() => this.handleSubmit('ecstatic')} alt="" src="/images/emojis/ecstatic-2.png"/>
+          <img
+            onClick={() => this.handleSubmit("sad")}
+            alt=""
+            src="/images/emojis/sad-2.png"
+          />
+          <img
+            onClick={() => this.handleSubmit("content")}
+            alt=""
+            src="/images/emojis/content-2.png"
+          />
+          <img
+            onClick={() => this.handleSubmit("ecstatic")}
+            alt=""
+            src="/images/emojis/ecstatic-2.png"
+          />
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
 const mapStateToProps = (state, ownProps) => {
-
   return {
     genreOne: state.currentGenres.genreOne,
     genreTwo: state.currentGenres.genreTwo,
     genreThree: state.currentGenres.genreThree,
     currentVibelist: state.currentVibelist,
     history: ownProps.history
-  }
-}
+  };
+};
 
 export default compose(
   withAuth,
   withRouter,
-  connect(mapStateToProps, actions),
+  connect(
+    mapStateToProps,
+    actions
+  )
 )(MoodEmojiSelector);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,21 @@
-import { combineReducers } from 'redux';
-import songs from './songs';
-import currentUser from './currentUser';
-import users from './users';
-import moods from './moods';
-import ecstaticSongs from './ecstaticSongs';
-import contentSongs from './contentSongs';
-import sadSongs from './sadSongs';
-import currentMood from './currentMood';
-import deviceId from './deviceId';
-import audioPlayer from './audioPlayer';
-import currentPlaylistUri from './currentPlaylistUri';
-import currentGenres from './currentGenres';
-import moodLists from './moodLists';
-import currentVibelist from './currentVibelist';
-import currentMoods from './currentMoods';
-import playlistSaved from './playlistSaved'
-
+import { combineReducers } from "redux";
+import songs from "./songs";
+import currentUser from "./currentUser";
+import users from "./users";
+import moods from "./moods";
+import ecstaticSongs from "./ecstaticSongs";
+import contentSongs from "./contentSongs";
+import sadSongs from "./sadSongs";
+import currentMood from "./currentMood";
+import deviceId from "./deviceId";
+import audioPlayer from "./audioPlayer";
+import currentPlaylistUri from "./currentPlaylistUri";
+import currentGenres from "./currentGenres";
+import moodLists from "./moodLists";
+import currentVibelist from "./currentVibelist";
+import currentMoods from "./currentMoods";
+import playlistSaved from "./playlistSaved";
+import playlist from "./playlist";
 
 export default combineReducers({
   songs,
