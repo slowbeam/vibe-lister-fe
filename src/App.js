6933dd21214e6a4e3029ecd716ad055f8748dc53
Sandy <sedwardscode@gmail.com>
2019-02-06 08:45:37 -0500

    reverted to version with page refresh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+import Login from './components/Login';
 import MoodEmojiSelector from './components/MoodEmojiSelector';
 import PlaylistContainer from './containers/PlaylistContainer';
 import MusicPlayer from './components/MusicPlayer';
 import LoginPage from './components/LoginPage';
 import WelcomePage from './components/WelcomePage';
 import MyVibeListsContainer from './containers/MyVibeListsContainer';
-import NavBar from './components/NavBar';
 import { connect } from 'react-redux';
 import * as actions from './actions';
 
@@ -18,126 +18,90 @@ class App extends Component {
 
 
   componentDidMount(){
-    this.storeAllData()
-  }
-
-  storeAllData = () => {
-    this.props.fetchSongs()
-    .then(() => {return this.props.fetchCurrentUser()})
-    .then(() => {return this.props.fetchMoods()})
-    .then(() => {return this.props.fetchUsers()})
-    .then(() => {return this.storeEcstaticMoodLists()})
-    .then(() => {return this.storeContentMoodLists()})
-    .then(() => {return this.storeSadMoodLists()})
-  }
+    this.props.fetchCurrentUser();
+  };
 
   setDisplayName = (user) => {
     if (localStorage.getItem('jwt') && this.props.currentUser) {
-      this.props.setDisplayName(user)
+      this.props.setDisplayName(user);
+    };
+  };
+
+  handleIconClick = (event) => {
+    event.preventDefault()
+    let hamburger = document.getElementById('hamburger');
+    if (hamburger.className === "fa fa-bars"){
+      hamburger.className = "fa fa-close"
+    } else {
+      hamburger.className = "fa fa-bars"
     }
-  }
+    let x = document.getElementById("mobile-nav-bar");
+    if (x.className === "mobile-nav-menu") {
+      x.className += " show tablet";
+    } else {
+      x.className = "mobile-nav-menu";
+    };
+  };
 
-  storeEcstaticMoodLists = () => {
-      if (this.props.currentUser !== null){
-        const userMoods = this.props.moods.filter(mood => mood.user_id === this.props.currentUser.id);
+  handleMenuClick = (event) => {
+    event.target.className = "active";
+  };
 
-        const userEcstaticMoods = userMoods.filter(mood => mood.name.includes("ecstatic"));
+  handleLogOut = () => {
+    this.props.setCurrentUser(null);
+    localStorage.removeItem('jwt');
+    window.location = 'https://www.spotify.com/logout/';
+    window.location = "http://localhost:3001/";
+  };
 
-        const ecstaticMoodsArray = [];
+  renderProfileImage = () => {
 
-        userEcstaticMoods.forEach((e,i) => (i = e.mood_list_id, ecstaticMoodsArray[i] ? ecstaticMoodsArray[i].push(e) : (ecstaticMoodsArray[i] = [e])));
-
-        const ecstaticMoodsNoEmpties = []
-        for (let el of ecstaticMoodsArray){
-          if (el) {
-            ecstaticMoodsNoEmpties.push(el)
-          }
-        }
-
-        const ecstaticMoodLists = [...ecstaticMoodsNoEmpties];
-
-        for (var i = 0; i < ecstaticMoodLists.length; i++) {
-
-          const newArray = {playlist_uri: "", saved: false, songs: []}
-
-          for (let el of ecstaticMoodLists[i]){
-            newArray.playlist_uri = ecstaticMoodLists[i][0].playlist_uri;
-            newArray.saved = el.saved;
-            let song = this.props.songs.find(song => song.id === el.song_id)
-            newArray.songs.push(song)
-          }
-           ecstaticMoodLists[i] = newArray
-        }
-          this.props.setEcstaticLists(ecstaticMoodLists)
-      }
-  }
-
-  storeContentMoodLists = () => {
-    if (this.props.currentUser !== null){
-      const userMoods = this.props.moods.filter(mood => mood.user_id === this.props.currentUser.id);
-
-      const userContentMoods = userMoods.filter(mood => mood.name.includes("content"));
-
-      const contentMoodsArray = [];
-
-      userContentMoods.forEach((e,i) => (i = e.mood_list_id, contentMoodsArray[i] ? contentMoodsArray[i].push(e) : (contentMoodsArray[i] = [e])));
-
-      const contentMoodsNoEmpties = []
-      for (let el of contentMoodsArray){
-        if (el) {
-          contentMoodsNoEmpties.push(el)
-        }
-      }
-
-      const contentMoodLists = [...contentMoodsNoEmpties];
-
-      for (var i = 0; i < contentMoodLists.length; i++) {
-
-        const newArray = {playlist_uri: "", saved: false, songs: []};
-
-        for (let el of contentMoodLists[i]){
-          newArray.playlist_uri = contentMoodLists[i][0].playlist_uri;
-          newArray.saved = el.saved;
-          let song = this.props.songs.find(song => song.id === el.song_id)
-          newArray.songs.push(song)
-        }
-         contentMoodLists[i] = newArray
-      }
-        this.props.setContentLists(contentMoodLists)
+    if (this.props.profileImage){
+      return this.props.profileImage
     }
-  }
-
-  storeSadMoodLists = () => {
-    if (this.props.currentUser !== null){
-      const userMoods = this.props.moods.filter(mood => mood.user_id === this.props.currentUser.id);
-      const userSadMoods = userMoods.filter(mood => mood.name.includes("sad"));
-
-      const sadMoodsArray = [];
-
-      userSadMoods.forEach((e,i) => (i = e.mood_list_id, sadMoodsArray[i] ? sadMoodsArray[i].push(e) : (sadMoodsArray[i] = [e])));
-
-      const sadMoodsNoEmpties = []
-      for (let el of sadMoodsArray){
-        if (el) {
-          sadMoodsNoEmpties.push(el)
-        }
-      }
-
-      const sadMoodLists = [...sadMoodsNoEmpties];
-
-      for (var i = 0; i < sadMoodLists.length; i++) {
-        const newArray = {playlist_uri: "", saved: false, songs: []};
-        for (let el of sadMoodLists[i]){
-          newArray.playlist_uri = sadMoodLists[i][0].playlist_uri;
-          newArray.saved = el.saved;
-          let song = this.props.songs.find(song => song.id === el.song_id)
-          newArray.songs.push(song)
-        }
-         sadMoodLists[i] = newArray
-      }
-        this.props.setSadLists(sadMoodLists)
+    else if (this.props.currentUser.profile_image){
+      return this.props.currentUser.profile_image
     }
-  }
+    else {
+      return "./images/avatar-icon.png"
+    }
+  };
+
+  renderLogInLogOut = () => {
+    const jwt = localStorage.getItem('jwt')
+    if (this.props.currentUser !== null & jwt !== null) {
+      return (
+        <div className="avatar-logout">
+          <div className="avatar"
+            style={{
+              backgroundImage: `url(${this.renderProfileImage()})`,
+              backgroundSize: "cover",
+              height: 60,
+              width: 60,
+              border: "3px solid #d09ed4",
+              borderRadius: "50%",
+            }}
+          />
+        <p onClick={this.handleLogOut}>logout</p>
+        </div>
+    )
+
+    } else {
+      return <Login />
+    }
+  };
+
+  renderLogInLogOutMobile = () => {
+    const jwt = localStorage.getItem('jwt')
+    if (this.props.currentUser !== null & jwt !== null) {
+      return (
+          <p onClick={this.handleLogOut}>logout</p>
+    )
+
+    } else {
+      return <Login />
+    }
+  };
 
   Login = () => {
     const token = localStorage.getItem('jwt');
@@ -148,55 +112,19 @@ class App extends Component {
     else {
       return <LoginPage />
     }
-
-  }
+  };
 
   Welcome = () => {
     return (
       <WelcomePage />
     )
-  }
+  };
 
   CreateNewVibeList = () => {
     return (
       <MoodEmojiSelector />
     )
-  }
-
-  CurrentPlaylistSad = () => {
-    return (
-      <div className="create-page-container">
-        <div className="create-page-content">
-          <PlaylistContainer />
-          <MusicPlayer />
-        </div>
-      </div>
-    )
-  }
-
-  CurrentPlaylistContent = () => {
-    return (
-      <div className="create-page-container">
-        <div className="create-page-content">
-          <PlaylistContainer />
-          <MusicPlayer />
-        </div>
-      </div>
-    )
-  }
-
-  CurrentPlaylistEcstatic = () => {
-    return (
-      <div className="create-page-container">
-        <div className="create-page-content">
-          <PlaylistContainer />
-          <MusicPlayer />
-        </div>
-      </div>
-
-
-    )
-  }
+  };
 
   CurrentVibelist = () => {
     return (
@@ -207,9 +135,7 @@ class App extends Component {
         </div>
       </div>
     )
-  }
-
-
+  };
 
   MyVibeLists = () => {
     return (
@@ -219,47 +145,57 @@ class App extends Component {
       </div>
 
     )
-  }
-
-
+  };
 
   render() {
     return (
+      <div className="page">
+        <div className="topnav" id="top-nav-bar">
+          <div className="box-1">
+            <a href="http://localhost:3001/" onClick={this.handleMenuClick} >home</a>
+            <a href="http://localhost:3001/my-vibelists" >my vibelists</a>
+            <a href="http://localhost:3001/create" >new vibelist</a>
+          </div>
+
+          <div className="box-2" >
+          </div>
+          <div className="box-3" >
+            {this.renderLogInLogOut()}
+            <a href="" className="icon" onClick={this.handleIconClick}>
+              <i id="hamburger" className="fa fa-bars"></i>
+            </a>
+          </div>
+        </div>
+        <div  id="mobile-nav-bar" className="mobile-nav-menu">
+          <a href="http://localhost:3001/" onClick={this.handleMenuClick} className="mobile-home-button">home</a>
+          <a href="http://localhost:3001/my-vibelists" >my vibelists</a>
+          <a href="http://localhost:3001/create" >new vibelist</a>
+          {this.renderLogInLogOutMobile()}
+        </div>
+
+
         <Router>
-          <div className="page">
-            <NavBar />
-            <div className="content">
-              <Route exact path="/" render={this.Login} />
-              <Route exact path="/welcome" render={this.Welcome} />
-              <Route exact path="/create" render={this.CreateNewVibeList} />
-              <Route exact path='/current-vibelist' render={this.CurrentVibelist} />
-              <Route exact path="/create-sad-vibelist" render={this.CurrentPlaylistSad} />
-              <Route exact path="/create-content-vibelist" render={this.CurrentPlaylistContent} />
-              <Route exact path="/create-ecstatic-vibelist" render={this.CurrentPlaylistEcstatic} />
-              <Route exact path="/my-vibelists" render={this.MyVibeLists} />
-            </div>
-            <div className="footer">
-              <p className="footer-text">created by Sandy Edwards</p>
-            </div>
+          <div className="content">
+            <Route exact path="/" render={this.Login} />
+            <Route exact path="/welcome" render={this.Welcome} />
+            <Route exact path="/create" render={this.CreateNewVibeList} />
+            <Route exact path='/current-vibelist' render={this.CurrentVibelist} />
+            <Route exact path="/my-vibelists" render={this.MyVibeLists} />
           </div>
         </Router>
-
+        <div className="footer">
+          <p className="footer-text">created by Sandy Edwards</p>
+        </div>
+      </div>
     );
   }
-}
+};
 
 const mapStateToProps = state => {
   return {
-    songs: state.songs,
-    users: state.users,
     currentUser: state.currentUser.user,
-    profileImage: state.currentUser.profileImage,
-    moods: state.moods,
-    sadLists: state.moodLists.sadLists,
-    contentLists: state.moodLists.contentLists,
-    ecstaticLists: state.moodLists.ecstaticLists,
-    moodListId: state.currentVibelist.mood_list_id
+    profileImage: state.currentUser.profileImage
   }
-}
+};
 
 export default connect(mapStateToProps, actions)(App);
